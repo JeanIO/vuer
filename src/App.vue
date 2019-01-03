a5a310f3df408237e06afeb619be05c04d2357f5
John <wangjiarui@mudu.tv>
2019-01-03 14:27:38 +0800

    finished the todoList

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+		<ToDoList>
+			<template slot="placement">
+				John's works
+			</template>
+		</ToDoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+		ToDoList
   }
 }
 </script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+		<ul v-if="toDoList.length">
+			<template v-for="(item, index) in toDoList">
+				<li :key="index" v-if="item.done" @click="changeState(item)"><del>{{item.value}}</del></li>
+				<li :key="index" v-else @click="changeState(item)">{{item.value}}</li>
+			</template>
+		</ul>
+	<div>
+		<input id="inputPlace"/>
+		<button v-on:click="addSomething">add sth</button>
+	</div>
+	<p><slot name="placement"></slot></p>
+	<!-- <p>{{message}}</p>
+	<input v-model="message"/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function(){
+		return {
+			toDoList: [],
+			message: ''
+
+		}
+	},
+	methods:{
+		addSomething: function() {
+			let value = document.getElementById('inputPlace').value
+			document.getElementById('inputPlace').value = ""
+			this.toDoList.push({
+				value,
+				done: false
+			})
+		},
+		changeState: function(item){
+			item.done = true
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
